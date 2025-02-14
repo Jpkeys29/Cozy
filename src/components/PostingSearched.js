6d095a8ccee57ca8ab0790d7cabafd49ce8c6da4
Jp Keys <jpmusik29@gmail.com>
2025-02-14 13:21:11 -0500

    PostinSearched styling

--- src/components/PostingSearched.js
+++ src/components/PostingSearched.js
@@ -1,24 +1,50 @@
 import React from "react";
 import Title from "./Title";
 import Banner from "./Banner";
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
 export default function PostingSearched() {
   const location = useLocation();
   const addressData = location.state || [];
   console.log('addressData:',addressData);
   return(
-    <div>
-      <h2>Places</h2>
-      <section>
+    <div className="flat-detail">
+      <div className="page-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+                <h1 className="page-title">Places</h1>
+                <h2 className="page-description">Best places in the area</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+            
+      <div className="container">
+        <div className="row">
         {addressData?.map((item, i) => (
-        <article key={i}>
-          <p>Neighborhood: {item.neighborhood}</p> 
+          <article key={i} className="text-center col-lg-4 col-12 col-md-6 ">
+            <div className="item-image">
+                    <img className="img-fluid" 
+                    src="/img/product1.jpeg" 
+                    // src={item.image} 
+                    alt="flat" />
+            </div>
+            <div className="d-flex justify-content-between mb-3">
+                        <span className="item-title">{item.neighborhood}</span>
+                        <span className="item-price">{item.price}</span>
+                    </div>
+            <div>
+
+            </div>
+        
+          {/* <p>Neighborhood: {item.neighborhood}</p>  */}
           <p>Description: {item.description} </p>
           <i>Price: {item.price}</i>
         </article>
       ))}
-      </section>
+        </div>
+      </div>
     </div>
   )
 }
